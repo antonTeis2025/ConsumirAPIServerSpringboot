dd20ad237f2dbad8656612e2986e6e26b8f260ea
LINKMASTER\conguchu <anton.davila.carro@gmail.com>
2025-12-18 18:23:18 +0100

    Initial commit

---
+++ src/test/java/anton/teis/consumirserver/ConsumirServerApplicationTests.java
@@ -0,0 +1,13 @@
+package anton.teis.consumirserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumirServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
